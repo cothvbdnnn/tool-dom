f84dad2ba75d95b5dda2bae96b3ca5be2e682a77
Thành <cothvbdnnn@gmail.com>
2021-07-07 08:54:44 +0700

    Update code.js

--- code.js
+++ code.js
@@ -27,16 +27,16 @@ let loop = setInterval(function() {
     const btnLike = $('.QBdPU')[1]
     const btnNext = $('.coreSpriteRightPaginationArrow')[0]
     const textLike = btnLike.getElementsByTagName('svg')[0].getAttribute("aria-label")
-    const iconBox = $('.X7cDz')[0].getElementsByClassName('wpO6b')[0]
-    const btnSubmit = $("button[type=submit]")[0]
     if (!btnLike || ["Unlike", "Bỏ thích"].includes(textLike)) {
         btnNext.click()
         return
     }
     btnLike.click()
+    const iconBox = $('.X7cDz')[0].getElementsByClassName('wpO6b')[0]
     iconBox.click()
     const arrayIcons = $('.CIwC8')
     arrayIcons[~~(Math.random() * 13)].click()
+    const btnSubmit = $("button[type=submit]")[0]
     btnSubmit.click()
     setTimeout(function() {
         btnNext.click()
@@ -55,10 +55,10 @@ const eventHover = new MouseEvent('mouseover', {
 })
 let loop = setInterval(function() {
     const btnNext = $('.coreSpriteRightPaginationArrow')[0]
-    const btnFollow = $('button.sqdOP.L3NKy._4pI4F.y3zKF')[1]
     if (!btnNext) return
     $('._6q-tv')[1].dispatchEvent(eventHover)
     setTimeout(function() {
+        const btnFollow = $('button.sqdOP.L3NKy._4pI4F.y3zKF')[1]
         if (btnFollow.innerHTML !== "Follow") {
             btnNext.click()
             return
